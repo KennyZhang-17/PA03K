2414c29a5159956900400b79c13272a368fc76ac
KennyZhang-17 <35554830+KennyZhang-17@users.noreply.github.com>
2018-03-15 15:28:25 -0700

    Add files via upload

--- cards.cpp
+++ cards.cpp
@@ -27,106 +27,124 @@ void Cards::clear(Card* n) {
     }
 }
 /*
-//destructor clears all cards in a stack (recursive)
-Cards::~Cards(){
-
-  delete root;
-}
-
-Cards::Card::~Card(){
+ //destructor clears all cards in a stack (recursive)
+ Cards::~Cards(){
+ 
+ delete root;
+ }
+ 
+ Cards::Card::~Card(){
  if(left!=0)
-   {delete (left);}
+ {delete (left);}
  if(right!=0)
-   {delete (right);}
-
-}
-
-*/
+ {delete (right);}
+ 
+ }
+ 
+ */
 //print all the cards in a stack
-void Cards::decode_s(Card *n){
+void Cards::decode_s(Card *n) const{
     int i=n->suit;
-    switch(i){
-    case 1: cout<< "c ";
-    case 2: cout<< "d ";
-    case 3: cout<< "s ";
-    case 4: cout<< "h ";
-    }
-
+        if (i==1) cout<< "c ";
+        else if (i==2) cout<< "d ";
+        else if (i==3) cout<< "s ";
+        else cout<< "h ";
 }
-void Cards::decode_n(Card *n){
+
+void Cards::decode_n(Card *n) const{
     int i=n->num;
-    switch(i){
-    case 11: cout<< "j"<<endl;
-    case 12: cout<< "q"<<endl;
-    case 13: cout<< "k"<<endl;
-    default: cout<< n->num<<endl;
-  
-    }      
-
-}
-void Cards::print() {
-    Card *n = root;
-    int j=root->suit;
-    int k=root->num;
-    while (n!=findPre(n,k,j)){
-        n=findPre(n,k,j);
-        k=n->num;
-        j=n->suit;
-    }
-    while (n!=findSuc(n,k,j)){
-      decode_s(n);
-      decode_n(n);
-        n=findSuc(n,k,j);
-        k=n->num;
-        j=n->suit;
-    }
+    if (i==1)  cout<< "a"<<endl;
+    else if (i==11) cout<< "j"<<endl;
+    else if (i==12) cout<< "q"<<endl;
+    else if (i==13) cout<< "k"<<endl;
+    else cout<< n->num<<endl;
 }
 
 //add a card to the end
-void Cards::append(char s, char k){
+bool Cards::append(char s, char k){
     int sig=0;
     int u=0;
-    switch(s){
-        case 'c': sig=1;
-        case 'd': sig=2;
-        case 's': sig=3;
-        case 'h': sig=4;
-    }
-    switch(k){
-        case 'j': u=11;
-        case 'q': u=12;
-        case 'k': u=13;
-    default: u=k-'0';
-    }
- 
-
-  
-    if (root == 0) { // empty list
+    //cout << "s: " << s << endl;
+    
+    if (s=='c') sig =1;
+    else if (s=='d') sig =2;
+    else if (s== 's') sig =3;
+    else  sig =4;
+    
+    if (k=='a') u = 1;
+    else if ( k=='j') u=11;
+    else if (k=='q') u=12;
+    else if (k== 'k') u=13;
+    else if (k==10) u=10;
+    else u = k-'0';
+    
+    if (!root){
         root = new Card;
-        root->suit = sig;
-        root->num = u;
+        root -> suit = sig;
+        root -> num = u;
+        root -> left = NULL;
+        root -> right = NULL;
+        return true;
     }
-    else {
-        Card *n = root;
-        while (n->left!=0 && n->right!=0){
-            if((sig<(n->suit)|| (sig==n->suit && (u<n->num || u==n->num))) && (n->left!=0) ){
-                n=n->left;
-            }
-            else{
-                n=n->right;
-            }
-        }
-        if(sig<(n->suit)|| (sig==n->suit && (u<n->num || u==n->num))){
-        n->left = new Card;
-        n->left->suit = sig;
-        n->left->num = u;
-        }
-        else{        
-        n->right = new Card;
-        n->right->suit = sig;
-        n->right->num = u;}
-    }
+    return append(sig, u, root);
 }
+
+bool Cards::append(int s, int k, Card* n){
+    int sig = s;
+    int u = k;
+    //cout << "sig: " << sig << endl;
+    //cout << "num: " << k << endl;
+    if (sig == n->suit){
+        if (u == n->num){
+            return false;
+        }else if (u < n -> num){
+            if (n->left)
+                return append(s,k,n->left);
+            else {
+                n -> left = new Card;
+                n -> left -> suit = sig;
+                n -> left -> num = u;
+                n -> left -> left = NULL;
+                n -> left -> right = NULL;
+                return true;
+            }
+        }else {
+            if (!n->right){
+                n->right = new Card;
+                n->right->suit = sig;
+                n->right->num = u;
+                n->right->left = NULL;
+                n->right->left = NULL;
+                return true;
+            }else{
+                return append(s,k,n->right);
+            }
+        }
+    }else if (sig>n->suit){
+        if (!n->right){
+            n->right = new Card;
+            n->right->suit = sig;
+            n->right->num = u;
+            n->right->left= NULL;
+            n->right->right = NULL;
+            return true;
+        }
+        else
+            return append(s,k,n->right);
+    }else{
+        if (!n->left){
+            n->left = new Card;
+            n->left->suit = sig;
+            n->left->num = u;
+            n->left->left= NULL;
+            n->left->right= NULL;
+            return true;
+        }else
+            return append(s,k,n->left);
+    }return false;
+}
+
+
 Card* Cards::findPre(Card* root, int num, int suit){
     if (root == NULL)  return 0;
     Card* pre;
@@ -148,7 +166,7 @@ Card* Cards::findPre(Card* root, int num, int suit){
         pre = root ;
         findPre(root->right, num, suit ) ;
     }
- 
+    
 }
 Card* Cards::findSuc(Card* root, int num, int suit){
     if (root == NULL)  return 0;
@@ -168,26 +186,26 @@ Card* Cards::findSuc(Card* root, int num, int suit){
     }
     if (root->suit < suit || root->num <=num)
     {
-         suc = root ;
+        suc = root ;
         findSuc(root->left, num, suit ) ;
     }
- 
+    
 }
 Card* Cards::deleteNode(Card* root, int suit, int num)
 {
     // base case
     if (root == NULL) return root;
- 
+    
     // If the key to be deleted is smaller than the root's key,
     // then it lies in left subtree
     if (suit < root->suit )
         root->left = deleteNode(root->left, suit,num);
- 
+    
     // If the key to be deleted is greater than the root's key,
     // then it lies in right subtree
     else if (suit > root->suit || (suit== root->suit && num<root->num))
         root->right = deleteNode(root->right, suit,num);
- 
+    
     // if key is same as root's key, then This is the node
     // to be deleted
     else
@@ -205,16 +223,16 @@ Card* Cards::deleteNode(Card* root, int suit, int num)
             clear(root);
             return temp;
         }
- 
+        
         // node with two children: Get the inorder successor (smallest
         // in the right subtree)
-
+        
         Card* temp = minValueNode(root->right);
- 
+        
         // Copy the inorder successor's content to this node
         root->suit = temp->suit;
         root->num = temp->num;
- 
+        
         // Delete the inorder successor
         root->right = deleteNode(root->right, temp->suit,temp->num);
     }
@@ -224,17 +242,17 @@ Card* Cards::deleteNode(Card* root, int suit, int num)
 Card* Cards::minValueNode(Card* s)
 {
     Card* current = s;
- 
+    
     /* loop down to find the leftmost leaf */
     while (current->left != NULL)
         current = current->left;
- 
+    
     return current;
 }
 
 //remove a card from a stack
 void Cards::remove(int suit, int k){
-
+    
     root=deleteNode(root,suit,k);
 }
 
@@ -258,24 +276,20 @@ bool Cards::matchFound(int suit, int k){
     return false;
 }
 //count the number of cards in the class
-int Cards::count()  {
-    int result = 0;
-    Card *n = root;
-    int j=root->suit;
-    int k=root->num;
-    while (n){
-        n=findPre(n,k,j);
-        k=n->num;
-        j=n->suit;
-    }
-    while (n){
-        ++result;
-        n=findSuc(n,k,j);
-        k=n->num;
-        j=n->suit;
-    }
-    return result;
+
+int Cards::count() const{
+    return count(root);
 }
+
+
+int Cards::count(Card*n) const{
+    if(!n) {
+        return 0;
+    } else {
+        return count(n->left) + count(n->right) + 1;
+    }
+}
+
 //Play the game and return the string of output
 string Cards::process_A(Cards & target){
     string result = "";
@@ -295,11 +309,11 @@ string Cards::process_A(Cards & target){
             target.remove(n->suit,n->num);
             this->remove(n->suit,n->num);
             break;
-        }         
+        }
         n=findSuc(n,k,j);
         k=n->num;
         j=n->suit;
-    } 
+    }
     return result;
 }
 string Cards::process_B(Cards & target){
@@ -320,10 +334,30 @@ string Cards::process_B(Cards & target){
             target.remove(n->suit,n->num);
             this->remove(n->suit,n->num);
             break;
-        }         
+        }
         n=findPre(n,k,j);
         k=n->num;
         j=n->suit;
-    } 
+    }
     return result;
 }
+
+
+void Cards::print() const{
+    print(root);
+}
+
+void Cards::print(Card* n) const{
+    if(n){
+        print(n->left);
+        //decode_s(n);
+        decode_n(n);
+        print(n->right);
+    }
+}
+
+
+
+
+
+
